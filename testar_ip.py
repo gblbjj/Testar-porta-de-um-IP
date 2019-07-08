0d0ac88f977cd8a7d9b6e5f7dfe1bc447611740f
gblbjj <oliveira992gabriel@gmail.com>
2019-07-07 21:20:17 -0300

    testar porta

--- testar_ip.py
+++ testar_ip.py
@@ -4,6 +4,6 @@ ip = raw_input("entre com o endereco do IP: ")
 port = input(" entre com o numero do porta: ")
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 if sock.connect_ex((ip,port)):
-    print " deu ruim", port, "esta fechada"
+    print (" deu ruim", port, "esta fechada")
 else:
-    print "a porta", port "esta aberta"    
+    print ("a porta", port "esta aberta" )   
